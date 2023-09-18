17a7dc78b8aaa6658668b0c23747c883f48c9cd8
NicoGarce <102405720+NicoGarce@users.noreply.github.com>
2023-09-18 13:17:50 +0800

    9/18/2023
    Linking order of libraries fixed

--- branch1/pages/suppliers.php
+++ branch1/pages/suppliers.php
@@ -33,8 +33,6 @@ $sups->delete_supp();
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
     <script defer src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <script defer src="/langgamtrading/assets/js/custom.js"></script>
-
     
     <link rel="stylesheet" href="/langgamtrading/css/main.css">
     <script src="/langgamtrading/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
@@ -44,7 +42,8 @@ $sups->delete_supp();
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
     <script defer src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-1.13.6/b-2.4.2/b-html5-2.4.2/b-print-2.4.2/datatables.min.js"></script>
-    
+    <script defer src="/langgamtrading/assets/js/custom.js"></script>
+
     <style>
         .dataTables_wrapper .dataTables_filter input[type="search"] {
             
--- branch2/pages/admin/sales.php
+++ branch2/pages/admin/sales.php
@@ -38,8 +38,6 @@ $sales->delete_sale();
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script defer src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <script defer src="/langgamtrading/assets/js/custom.js"></script>
-
 
     <link rel="stylesheet" href="/langgamtrading/css/main.css">
     <script src="/langgamtrading/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
@@ -49,6 +47,7 @@ $sales->delete_sale();
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
     <script defer src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-1.13.6/b-2.4.2/b-html5-2.4.2/b-print-2.4.2/datatables.min.js"></script>
+    <script defer src="/langgamtrading/assets/js/custom.js"></script>
 
     <style>
         .dataTables_wrapper .dataTables_filter input[type="search"] {
