69d69a9612f6827c73a4a6013ec84e15886e6572
NicoGarce <102405720+NicoGarce@users.noreply.github.com>
2023-09-18 13:56:42 +0800

    add products update
    stock and price input fix

--- branch1/pages/modals/add_product.php
+++ branch1/pages/modals/add_product.php
@@ -1,50 +1,42 @@
 <div>
 
-    <div class="modal fade" id="addProduct" tabindex="-1" aria-labelledby="label" data-bs-backdrop="static"
-        data-bs-keyboard="false" aria-hidden="true">
+    <div class="modal fade" id="addProduct" tabindex="-1" aria-labelledby="label" data-bs-backdrop="static" data-bs-keyboard="false" aria-hidden="true">
         <div class="modal-dialog pt-5">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="labelNew">Add Product</h5>
-                    <button type="button" id="headClose" class="btn-close" data-bs-dismiss="modal"
-                        aria-label="Close"></button>
+                    <button type="button" id="headClose" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <form method="post" id="registration-form">
 
                         <div class="form-group pt-2">
-                            <input type="text" class="form-control" id="product_name" name="product_name"
-                                placeholder="Product Name" required>
+                            <input type="text" class="form-control" id="product_name" name="product_name" placeholder="Product Name" required>
                         </div>
 
                         <div class="row pt-2">
                             <div class="form-group col-md-6">
-                                <input class="form-control" id="stock" name="stock" placeholder="Stock"
-                                    required></input>
+                                <input type="number" class="form-control" id="stock" name="stock" placeholder="Stock" required></input>
                             </div>
                             <div class="form-group col-md-6 ">
-                                <input class="form-control" id="price" name="price" placeholder="Price"
-                                    required></input>
+                                <input class="form-control" id="price" name="price" placeholder="Price" required></input>
                             </div>
                         </div>
 
                         <div class="form-group pt-2">
-                            <input type="text" class="form-control" id="category" name="category" placeholder="Category"
-                                required>
+                            <input type="text" class="form-control" id="category" name="category" placeholder="Category" required>
                         </div>
                         <div class="form-group pt-2">
                             <label for="date_ordered" class="label small px-1">Date Ordered</label>
-                            <input type="date" class="form-control" id="date_ordered" name="date_ordered"
-                                placeholder="Date Ordered" required>
+                            <input type="date" class="form-control" id="date_ordered" name="date_ordered" placeholder="Date Ordered" required>
                         </div>
                         <div class="form-group pt-2">
                             <label for="date_arrival" class="label small px-1">Date of Arrival</label>
-                            <input type="date" class="form-control" id="date_arrival" name="date_arrival"
-                                placeholder="Date Arrival" required>
+                            <input type="date" class="form-control" id="date_arrival" name="date_arrival" placeholder="Date Arrival" required>
                         </div>
 
                         <?php
-                            $suppliers = $sups->get_suppliers();
+                        $suppliers = $sups->get_suppliers();
                         ?>
                         <div class="form-group pt-2 pb-4">
                             <select class="form-control" id="supplier_id" name="supplier_id" required>
@@ -59,8 +51,7 @@
 
 
                         <div class="modal-footer">
-                            <button type="button" id="footClose" class="btn btn-secondary"
-                                data-bs-dismiss="modal">Close</button>
+                            <button type="button" id="footClose" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             <button type="submit" name="add_product" class="btn btn-primary">Add Product</button>
                         </div>
                     </form>
@@ -71,3 +62,33 @@
 </div>
 <?php $inventory->add_product(); ?>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script>
+    // Get a reference to the input element
+    var stockInput = document.getElementById('stock');
+    var priceInput = document.getElementById('price');
+
+    priceInput.addEventListener('input' , function(){
+        var currentPrice = parseFloat(priceInput.value);
+
+        // Check if the input value is not a number or is negative
+        if (currentPrice  < 0) {
+            // If it's not a number or is negative, set the input value to 0
+            priceInput.value = 0;
+        }
+    });
+
+    // Add an event listener for the 'input' event
+    stockInput.addEventListener('input', function() {
+        // Get the current value of the input
+        var currentValue = parseFloat(stockInput.value);
+
+        // Check if the input value is not a number or is negative
+        if (isNaN(currentValue) || currentValue < 0) {
+            // If it's not a number or is negative, set the input value to 0
+            stockInput.value = 0;
+        } else {
+            // If it's a positive number, round it to the nearest integer
+            stockInput.value = Math.round(currentValue);
+        }
+    });
+</script>
