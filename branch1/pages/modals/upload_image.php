3fe687a1095c9e96ec0ad8eac8d4c88a9d58868d
NicoGarce <102405720+NicoGarce@users.noreply.github.com>
2023-11-15 00:22:46 +0800

    upload image update
    1x1 only for modal
    modal reset once user closes

--- branch1/pages/modals/upload_image.php
+++ branch1/pages/modals/upload_image.php
@@ -9,13 +9,13 @@ $profile->upload_pic($id);
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="labelEdit">Upload Photo | Choose a 1x1 Photo </h5>
-                <button type="button" id="headClose<?php echo $cnt ?>" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" id="headClose" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form method="post" enctype="multipart/form-data">
+            <form method="post" enctype="multipart/form-data" id="upload-form">
                 <div class="modal-body">
+                    <div id="error-message" class="text-danger d-flex justify-content-center pb-2"></div>
                     <div class="pb-2 d-flex justify-content-center">
-                        <img src="<?php echo (!empty($result[0]->photo)) ? '../' . $result[0]->photo : '/langgamtrading/assets/user_upload/default.png' ?>" 
-                            alt="photo" id="display" class="img-fluid border border-2 rounded-circle" width="250px" height="250px">
+                        <img src="<?php echo (!empty($result[0]->photo)) ? '../' . $result[0]->photo : '/langgamtrading/assets/user_upload/default.png' ?>" alt="photo" id="display" class="img-fluid border border-2 rounded-circle" width="250px" height="250px">
                     </div>
                     <div class="d-flex justify-content-center mx-auto">
                         <label for="file" class="btn btn-primary">Choose Photo</label>
@@ -32,26 +32,59 @@ $profile->upload_pic($id);
 </div>
 
 <script>
-   // Define checkFile function globally
-   function checkFile() {
-       var fileInput = document.querySelector('input[name="file"]');
-       var uploadButton = document.querySelector('button[name="upload"]');
+    function checkFile() {
+        var fileInput = document.querySelector('input[name="file"]');
+        var uploadButton = document.querySelector('button[name="upload"]');
+        var errorMessage = document.getElementById('error-message');
+        var profilePic = document.getElementById("display");
 
-       // Check if a file is selected
-       if (fileInput.files.length > 0) {
-           uploadButton.disabled = false; // Enable the button
-       } else {
-           uploadButton.disabled = true; // Disable the button
-       }
-   }
+        if (fileInput.files.length > 0) {
+            var reader = new FileReader();
 
-   $(document).ready(function() {
-       let profilePic = document.getElementById("display");
-       let inputFile = document.getElementById("file");
+            reader.onload = function(e) {
+                var img = new Image();
+                img.src = e.target.result;
 
-       inputFile.onchange = function() {
-           profilePic.src = URL.createObjectURL(inputFile.files[0]);
-           checkFile(); // Call the function every time a file is selected
-       }
-   });
+                img.onload = function() {
+                    if (img.width === img.height) {
+                        uploadButton.disabled = false;
+                        errorMessage.innerText = '';
+                        profilePic.src = URL.createObjectURL(fileInput.files[0]);
+                    } else {
+                        uploadButton.disabled = true;
+                        errorMessage.innerText = 'Please choose a 1x1 photo.';
+                    }
+                };
+            };
+
+            reader.readAsDataURL(fileInput.files[0]);
+        } else {
+            uploadButton.disabled = true;
+            errorMessage.innerText = '';
+        }
+    }
+
+    function resetForm() {
+        var fileInput = document.getElementById("file");
+        var errorMessage = document.getElementById("error-message");
+        var profilePic = document.getElementById("display");
+
+        fileInput.value = "";
+        errorMessage.innerText = "";
+        document.getElementById("upload-form").reset();
+        profilePic.src = "<?php echo (!empty($result[0]->photo)) ? '../' . $result[0]->photo : '/langgamtrading/assets/user_upload/default.png' ?>";
+        document.querySelector('button[name="upload"]').disabled = true;
+    }
+
+    $(document).ready(function() {
+        let inputFile = document.getElementById("file");
+
+        inputFile.onchange = function() {
+            checkFile();
+        }
+
+        $('#upload').on('hidden.bs.modal', function() {
+            resetForm();
+        });
+    });
 </script>
