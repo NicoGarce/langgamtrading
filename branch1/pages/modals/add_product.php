e20f8ece94b380644ec1f753753700a6ee6a2931
NicoGarce <102405720+NicoGarce@users.noreply.github.com>
2023-11-30 16:44:55 +0800

    Update add_product.php

--- branch1/pages/modals/add_product.php
+++ branch1/pages/modals/add_product.php
@@ -11,35 +11,35 @@
                     <form method="post" id="registration-form">
 
                         <div class="form-group pt-2">
-                            <input type="text" class="form-control" id="product_name" name="product_name" placeholder="Product Name" required autocomplete="off">
+                            <input type="text" class="form-control" id="product_name" name="product_name" placeholder="Product Name" required>
                         </div>
 
                         <div class="row pt-2">
                             <div class="form-group col-md-6">
-                                <input type="number" class="form-control" id="stock" name="stock" placeholder="Stock" oninput="if (this.value < 0 || this.value.includes('.')) this.value = 0;" required autocomplete="off"></input>
+                                <input type="number" class="form-control" id="stock" name="stock" placeholder="Stock" oninput="if (this.value < 0 || this.value.includes('.')) this.value = 0;" required></input>
                             </div>
                             <div class="form-group col-md-6 ">
-                                <input type="number" class="form-control" id="price" name="price" placeholder="Price" required autocomplete="off" oninput="if (this.value < 0) this.value = 0;" step="any">
+                                <input type="number" class="form-control" id="price" name="price" placeholder="Price" required oninput="if (this.value < 0) this.value = 0;" step="any">
                             </div>
                         </div>
 
                         <div class="form-group pt-2">
-                            <input type="text" class="form-control" id="category" name="category" placeholder="Category" required autocomplete="off">
+                            <input type="text" class="form-control" id="category" name="category" placeholder="Category" required>
                         </div>
                         <div class="form-group pt-2">
                             <label for="date_ordered" class="label small px-1">Date Ordered</label>
-                            <input type="date" class="form-control" id="date_ordered" name="date_ordered" placeholder="Date Ordered" required autocomplete="off">
+                            <input type="date" class="form-control" id="date_ordered" name="date_ordered" placeholder="Date Ordered" required>
                         </div>
                         <div class="form-group pt-2">
                             <label for="date_arrival" class="label small px-1">Date of Arrival</label>
-                            <input type="date" class="form-control" id="date_arrival" name="date_arrival" placeholder="Date Arrival" required autocomplete="off">
+                            <input type="date" class="form-control" id="date_arrival" name="date_arrival" placeholder="Date Arrival" required>
                         </div>
 
                         <?php
                         $suppliers = $sups->get_suppliers();
                         ?>
                         <div class="form-group pt-2 pb-4">
-                            <select class="form-control" id="supplier_id" name="supplier_id" required autocomplete="off">
+                            <select class="form-control" id="supplier_id" name="supplier_id" required>
                                 <option value="">--Select Supplier--</option>
                                 <?php
                                 foreach ($suppliers as $supplier) {
